af2457f4b71cee90a68eddddbefa720ecb7478c6
waiser86 <d.alimoff@mail.ru>
2020-11-19 10:03:21 +0500

    lavender: include Gcam

--- device.mk
+++ device.mk
@@ -146,7 +146,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64
+    vendor.qti.hardware.camera.device@1.0:64 \
+    Gcam
 
 # Camera  Shims
 PRODUCT_PACKAGES += \
